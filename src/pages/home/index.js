c686b9ead683cc901e4e4f04715e3742470e8c2f
silvia_phungky <silvia@qlue.id>
2021-05-27 22:28:50 +0700

    create page

---
+++ src/pages/home/index.js
@@ -0,0 +1,66 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState, useCallback } from 'react'
+import MovieApi from '../../services/movie-api'
+import {
+  Layout, 
+  NowPlayingMovie,
+  SearchMovie,
+  FilteredMovie,
+  PreviewMovieDetail,
+  TopRatedMovie,
+  PopularMovie
+} from 'components/organisms'
+
+import { useLocation } from 'react-router-dom'
+import { useFilter } from 'context/filter-provider'
+
+const HomePage = () => {
+
+  const [activeSection, setActiveSection] = useState('Now Playing')
+  const [movieDetail, setMovieDetail] = useState({})
+  const [keyword, setKeyword] = useState('')
+  const location = useLocation()
+  const { yearFilter } = useFilter()
+
+
+
+  useEffect(() => {
+    const searchKeyword = location.search.split('=').pop()
+    setKeyword(searchKeyword)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.search])
+
+
+  const fetchMovieDetail = ({ id }) => {
+    MovieApi.getMovieDetail({ id })
+      .then((response) => {
+        const data = response.data
+        setMovieDetail(data)
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const displayMovieList = () => {
+    if(keyword) return <SearchMovie fetchMovieDetail={ fetchMovieDetail } />
+    else if (yearFilter) return <FilteredMovie fetchMovieDetail={ fetchMovieDetail } />
+    else {
+      if(activeSection === 'Now Playing') return <NowPlayingMovie fetchMovieDetail={ fetchMovieDetail }/>
+      else if(activeSection === 'Top Rated') return <TopRatedMovie fetchMovieDetail={ fetchMovieDetail }/>
+      else return <PopularMovie fetchMovieDetail={ fetchMovieDetail }/>
+    }
+  }
+
+  return(
+    <>
+      <Layout 
+        activeSection={ activeSection } 
+        setActiveSection={ setActiveSection }
+      >
+        <PreviewMovieDetail detail={ movieDetail } />
+        { displayMovieList() }
+      </Layout>
+    </>
+  )
+}
+
+export default HomePage
